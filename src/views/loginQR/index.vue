d24f6bd63836d9221c4a1aa8be5cb7207596c1e6
YUE TANG <yueteresatang@gmail.com>
2022-08-08 09:20:03 +0800

    feat: 懒加载和二维码登陆

---
+++ src/views/loginQR/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-page">
+    key:  {{ keyQR&&keyQR }}
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { reactive } from "vue";
+import { ref } from "vue";
+
+import { getQRKey } from "../../network/request";
+
+const keyQR= ref(''); //切割类型
+
+async function getKey () {
+  let data = await getQRKey();
+  keyQR.value = data;
+}
+
+async function init() {
+  getKey();
+}
+
+init();
+</script>
+
+<style scoped lang="scss">
+.login-page {
+  margin: auto;
+  width: 300px;
+}
+</style>
---
+++ src/views/musicList/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home" />
+</template>
+<script setup lang='ts' scoped>
+// import { reactive, computed } from 'vue';
+
+import {
+getRecommendsong
+} from '../../network/request';
+
+// let state = reactive({
+//   privateContent: { privateContentList: [], title: '' },
+//   personlized: { personlizedList: [], title: '推荐歌单' },
+//   recommend: { recommendList: [], title: '独家放送2.0' },
+//   newSong: { songList: [], title: '最新音乐' },
+// });
+
+async function getRecSong() {
+  let data = await getRecommendsong();
+  console.log('~~~~~~~~~~~~~~~~');
+  console.log(data);
+  // state.privateContent.privateContentList = data?.result || [];
+  // state.privateContent.title = data?.name;
+}
+
+
+
+async function init() {
+  getRecSong();
+}
+
+init();
+</script>
+<style scoped lang='scss' src='./index.scss' />
