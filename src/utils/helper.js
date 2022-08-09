09e76e9c17e4201dbc2d91ca0548885b3478629e
YUE TANG <yueteresatang@gmail.com>
2022-08-09 17:05:52 +0800

    music list icons

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,3 +2,14 @@ export function toTenThousand(num) {
     return num < 10000 ?
         num : Math.floor(num / 10000) + "万";
 }
+
+export function modMusicList(list) {
+    const modList = list.map((item, index) => ({
+        order: index + 1 < 10 ? '0' + (index + 1) : index + 1,
+        name: item.name,
+        artist: item.ar[0].name || '',
+        album: item.al.name || '',
+        isLike: false
+    }));
+    return modList
+}
--- src/views/musicList/index.vue
+++ src/views/musicList/index.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="home" />
+  <el-table
+    :data="state.dailySongs"
+    stripe
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="order"
+      width="80"
+    />
+    <el-table-column
+      label=""
+      width="180"
+    >
+      <template #default="{row}">
+        <!-- :class="['common-modal-title', `common-modal-title-${titleClass}`] -->
+        <div class="operation">
+          <!-- <div :class=" row.isLike ? 'like':'unlike' " /> -->
+          <div
+            :class="['unlike', row.isLike ? 'like':'']"
+          />
+          <div class="download" />
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="音乐标题"
+      width="180"
+    />
+    <el-table-column
+      prop="artist"
+      label="歌手"
+      width="180"
+    />
+    <el-table-column
+      prop="album"
+      label="专辑"
+    />
+  </el-table>
 </template>
 <script setup lang='ts' scoped>
 import { reactive} from 'vue';
@@ -7,6 +45,7 @@ import { reactive} from 'vue';
 import {
 getRecommendsong
 } from '../../network/request';
+import { modMusicList } from '../../utils/helper';
 
 let state = reactive({
   dailySongs: [],
@@ -15,12 +54,10 @@ let state = reactive({
 
 async function getRecSong() {
   let data = await getRecommendsong();
-  const {dailySongs=[], orderSongs=[]} = data
-  state.dailySongs = dailySongs;
+  const { dailySongs = [], orderSongs = [] } = data
+  state.dailySongs = modMusicList(dailySongs);
   state.orderSongs = orderSongs;
 }
-
-
 
 async function init() {
   getRecSong();
